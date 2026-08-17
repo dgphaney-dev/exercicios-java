7b4a6c0b8b1c4371f8d07cff2f1e0cdd7cde7d65
Douglas Phaney <douglasphalbuquerque@gmail.com>
2026-08-17 16:09:24 -0300

    Adiciona exercicio 2 - fizzbuzz

---
+++ exercicio2.java
@@ -0,0 +1,16 @@
+// Imprima os números de 1 a 50. Para múltiplos de 3,
+// imprima "Fizz"; para múltiplos de 5,
+// "Buzz"; para múltiplos de ambos (3 e 5),
+// "FizzBuzz"; e para múltiplos de 7 também, 
+// imprima "Bang".
+
+import java.util.Scanner;
+
+public class exercicio2 {
+    public static void main(String[] args) {
+        
+    for (int i = 1; i <= 10; i++) {
+    System.out.println(i);
+}
+    }
+}
